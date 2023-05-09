cabbd761eb3be7f6b34ed7d9bcb31acde3ddfe3f
NewAmazingPVP <gamingofpat@gmail.com>
2023-05-08 20:04:15 -0400

    setdelay and config work correctly now ready for release
    setdelay and config work correctly now ready for release

--- src/main/java/newamazingpvp/nohitdelay/NoHitDelay.java
+++ src/main/java/newamazingpvp/nohitdelay/NoHitDelay.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -14,18 +15,22 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NoHitDelay extends JavaPlugin implements Listener {
-    public long Delay = 2L;
+    public FileConfiguration config;
+
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+        config = getConfig();
     }
 
     @EventHandler
     private void onEntityDamage(EntityDamageByEntityEvent event) {
+        long hitDelay = config.getLong("delay");
         Entity entity = event.getEntity();
         if (entity instanceof LivingEntity) {
-            LivingEntity livingEntity = (LivingEntity)entity;
-            Bukkit.getScheduler().runTaskLater((Plugin)this, () -> livingEntity.setNoDamageTicks(0), Delay);
+            LivingEntity livingEntity = (LivingEntity) entity;
+            Bukkit.getScheduler().runTaskLater((Plugin) this, () -> livingEntity.setNoDamageTicks(0), hitDelay);
         }
     }
 
@@ -33,16 +38,17 @@ public final class NoHitDelay extends JavaPlugin implements Listener {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (args.length == 1) {
+            if (args.length == 1 && command.getName().equalsIgnoreCase("setdelay")) {
                 try {
                     long delay = Long.parseLong(args[0]);
-                    Delay = delay;
-                    player.sendMessage("Delay set to: " + ChatColor.GREEN + delay + "Do make sure the delay is at least 2 because setting it below that will make some hits not register");
+                    config.set("delay", delay);
+                    saveConfig();
+                    player.sendMessage(ChatColor.GREEN + "Delay set to: " + ChatColor.YELLOW + delay + ChatColor.RESET + ". Do make sure the delay is at least 2 because setting it below that will make some hits not register.");
                 } catch (NumberFormatException e) {
-                    player.sendMessage("Invalid delay value. Please enter a number.");
+                    player.sendMessage(ChatColor.RED + "Invalid delay value. Please enter a number.");
                 }
             } else if (args.length == 0 && command.getName().equalsIgnoreCase("getdelay")) {
-                player.sendMessage("Delay is currently set to: " + ChatColor.GREEN + Delay);
+                player.sendMessage(ChatColor.GREEN + "Delay is currently set to: " + ChatColor.YELLOW + config.getLong("delay"));
             }
         }
         return true;
